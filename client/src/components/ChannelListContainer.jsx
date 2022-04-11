3068ecc10971b54afe005fc4701b1472e41443bc
nipengg <nevillecorneliustj@gmail.com>
2022-04-11 13:14:15 +0700

    create search and channel list component

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -33,6 +33,27 @@ const ChannelListContainer = () => {
       <SideBar />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+            filters={{}}
+            channelRenderFilterFn={() => {}}
+            List={(listProps) => (
+              <TeamChannelList {...listProps} type="team" />
+            )}
+            Preview={(previewProps) => (
+              <TeamChannelPreview {...previewProps} type="team" />
+            )}
+        />
+        <ChannelList
+            filters={{}}
+            channelRenderFilterFn={() => {}}
+            List={(listProps) => (
+              <TeamChannelList {...listProps} type="messaging" />
+            )}
+            Preview={(previewProps) => (
+              <TeamChannelPreview {...previewProps} type="messaging" />
+            )}
+        />
       </div>
     </>
   )
