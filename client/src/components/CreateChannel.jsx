eeb765b1f03c2d83b76f9cb6512e40201e54c336
nipengg <nevillecorneliustj@gmail.com>
2022-04-12 13:45:39 +0700

    finishing create channel component

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -6,22 +6,43 @@ import { CloseCreateChannel } from '../assets';
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
   const handleChange = (event) => {
     event.preventDefault();
-    setChannelName=(event.target.value);
+    setChannelName(event.target.value);
   }
 
   return (
     <div className="channel-name-input__wrapper">
       <p>Name</p>
-      <input value={channelName} onChange={handleChange} placeholder='Channel name' />
+      <input value={channelName} onChange={handleChange} placeholder="channel-name" />
       <p>Add Members</p>
     </div>
-  );
+  )
 }
 
 const CreateChannel = ({ createType, setIsCreating }) => {
   const { client, setActiveChannel } = useChatContext();
   const [selectedUsers, setSelectedUsers] = useState([client.userID || '']);
   const [channelName, setChannelName] = useState('');
+
+  const createChannel = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newChannel = await client.channel(createType, channelName, {
+        name: channelName,
+        members: selectedUsers,
+      });
+
+      await newChannel.watch();
+
+      setChannelName('');
+      setIsCreating(false);
+      setSelectedUsers([client.userID]);
+      setActiveChannel(newChannel);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="create-channel__container">
@@ -31,6 +52,9 @@ const CreateChannel = ({ createType, setIsCreating }) => {
       </div>
       {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
       <UserList setSelectedUsers={setSelectedUsers} />
+      <div className="create-channel__button-wrapper" onClick={createChannel}>
+        <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+      </div>
     </div>
   )
 }
